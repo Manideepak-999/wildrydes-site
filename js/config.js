8114d166cafd0a8eedd41ed0fbc16382ae9b030a
Vemuri Manideepak <83538320+Manideepak-999@users.noreply.github.com>
2024-04-30 22:25:16 -0500

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_fEkCkjUfx', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3de41o2bk1t94633ogottkiv14', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_2WWfKxadp', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4ts4071shujfs0u6q8qiqjnnq8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1"new_config"' // e.g. us-east-2
     },
     api: {
